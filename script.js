d4c216645fa82aa4addbfa56433e10fefe429804
Emily Leff <ecleff@gmail.com>
2023-02-15 14:32:45 -0500

    new csv with county and season grouping

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ const svg = d3.select("#my_dataviz")
   .append("g")
     .attr("transform", `translate(${width/2},${height/2+100})`); // Add 100 on Y translation, cause upper bars are longer
 
-    d3.csv("Bigfoot Sightings in MA - Sheet1.csv").then(function (data) {
+    d3.csv("bigfoot.csv").then(function (data) {
         data.forEach(function (d) {
-            d["Number of Witnesses"] = +d["Number of Witnesses"];
-            d["Sightings per Season"] = +d["Sightings per Season"];
+            d["Number.of.Witnesses"] = +d["Number.of.Witnesses"];
+            d["Sightings_by_Season"] = +d["Sightings_by_Season"];
           });
 
   // X scale
   const x = d3.scaleBand()
       .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
       .align(0)                  // This does nothing ?
-      .domain( data.map(d => d.Index)); // The domain of the X axis is the list of states.
+      .domain( data.map(d => d.index)); // The domain of the X axis is the list of states.
 
   // Y scale
   const y = d3.scaleRadial()
@@ -34,14 +34,14 @@ const svg = d3.select("#my_dataviz")
  // Second barplot Scales
  var ybis = d3.scaleRadial()
  .range([innerRadius, 5])   // Domain will be defined later.
- .domain([0, 10]);
+ .domain([0, 5]);
 
 // Seasons color scale
 var seasonsScale = d3.scaleOrdinal().domain(data)
   .range(["#2f52e0", "#bced09", "#f9cb40", "#ff715b"])
 // Environment color scale
 var enviroScale = d3.scaleOrdinal().domain(data)
-  .range(["#beb0a7", "#8b9d83", "#6a7b76", "#3a4e48", "#040303"])
+  .range(["#e5f0e8", "#b1d2ba", "#71ad81", "#4a8259", "#25412d"])
 
   // Add bars
   svg.append("g")
@@ -51,28 +51,29 @@ var enviroScale = d3.scaleOrdinal().domain(data)
     .attr("fill", function (d) {
         return enviroScale(d.Environment);
       })
+      .attr("stroke", "black")
       .attr("d", d3.arc()     // imagine your doing a part of a donut plot
           .innerRadius(innerRadius)
-          .outerRadius(d => y(d['Number of Witnesses']))
-          .startAngle(d => x(d.Index))
-          .endAngle(d => x(d.Index) + x.bandwidth())
+          .outerRadius(d => y(d['Number.of.Witnesses']))
+          .startAngle(d => x(d.index))
+          .endAngle(d => x(d.index) + x.bandwidth())
           .padAngle(0.01)
           .padRadius(innerRadius))
 
 
     // Add the labels
-  svg.append("g")
-  .selectAll("g")
-  .data(data)
-  .enter()
-  .append("g")
-    .attr("text-anchor", function(d) { return (x(d.Index) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-    .attr("transform", function(d) { return "rotate(" + ((x(d.Index) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Number of Witnesses'])+10) + ",0)"; })
-  .append("text")
-    .text(function(d){return(d.County)})
-    .attr("transform", function(d) { return (x(d.County) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-    .style("font-size", "11px")
-    .attr("alignment-baseline", "middle")
+//   svg.append("g")
+//   .selectAll("g")
+//   .data(data)
+//   .enter()
+//   .append("g")
+//     .attr("text-anchor", function(d) { return (x(d.index) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+//     .attr("transform", function(d) { return "rotate(" + ((x(d.index) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Number.of.Witnesses'])+10) + ",0)"; })
+//   .append("text")
+//     .text(function(d){return(d.County)})
+//     .attr("transform", function(d) { return (x(d.County) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+//     .style("font-size", "11px")
+//     .attr("alignment-baseline", "middle")
 
 // Add the second series
 svg.append("g")
@@ -83,12 +84,21 @@ svg.append("g")
 .attr("fill", function (d) {
     return seasonsScale(d.Season);
   })
+  .attr("stroke", "grey")
   .attr("d", d3.arc()     // imagine your doing a part of a donut plot
       .innerRadius( function(d) { return ybis(0) })
-      .outerRadius( function(d) { return ybis(d['Sightings per Season']); })
-      .startAngle(function(d) { return x(d.Index); })
-      .endAngle(function(d) { return x(d.Index) + x.bandwidth(); })
+      .outerRadius( function(d) { return ybis(d['Sightings_by_Season']); })
+      .startAngle(function(d) { return x(d.index); })
+      .endAngle(function(d) { return x(d.index) + x.bandwidth(); })
       .padAngle(0.01)
       .padRadius(innerRadius))
+
+    //   d3.arc()
+    //   .innerRadius(d => yScale(d.min))
+    //   .outerRadius(d => yScale(d.max))
+    //   .startAngle(d => xScale(d.Index))
+    //   .endAngle(d => xScale(d.Index) + xScale.bandwidth())
+    //   .padAngle(0.01)
+    //   .padRadius(innerRadius)
 
 });
