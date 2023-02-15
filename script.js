63ae67350956c8e8c8e10d94ef555eeeefdda32c
Emily Leff <ecleff@gmail.com>
2023-02-15 11:22:45 -0500

    adding colors for seasons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = 460 - margin.left - margin.right,
-    height = 460 - margin.top - margin.bottom,
-    innerRadius = 80,
+const margin = {top: 10, right: 10, bottom: 150, left: 10},
+    width = 1000 - margin.left - margin.right,
+    height = 1000 - margin.top - margin.bottom,
+    innerRadius = 100,
     outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
 
 // append the svg object to the body of the page
@@ -28,13 +28,18 @@ const svg = d3.select("#my_dataviz")
   const y = d3.scaleRadial()
       .range([innerRadius, outerRadius])   // Domain will be define later.
       .domain([0, 5]); // Domain of Y is from 0 to the max seen in the data
+// color scale
+var myColor = d3.scaleOrdinal().domain(data)
+  .range(["blue", "pink", "yellow", "red"])
 
   // Add bars
   svg.append("g")
     .selectAll("path")
     .data(data)
     .join("path")
-      .attr("fill", "#69b3a2")
+      .attr("fill", function (d) {
+        return myColor(d.Season);
+      })
       .attr("d", d3.arc()     // imagine your doing a part of a donut plot
           .innerRadius(innerRadius)
           .outerRadius(d => y(d['Number of Witnesses']))
@@ -42,5 +47,21 @@ const svg = d3.select("#my_dataviz")
           .endAngle(d => x(d.Index) + x.bandwidth())
           .padAngle(0.01)
           .padRadius(innerRadius))
+
+
+    // Add the labels
+  svg.append("g")
+  .selectAll("g")
+  .data(data)
+  .enter()
+  .append("g")
+    .attr("text-anchor", function(d) { return (x(d.Index) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+    .attr("transform", function(d) { return "rotate(" + ((x(d.Index) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Number of Witnesses'])+10) + ",0)"; })
+  .append("text")
+    .text(function(d){return(d.County)})
+    .attr("transform", function(d) { return (x(d.County) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+    .style("font-size", "11px")
+    .attr("alignment-baseline", "middle")
+
 
 });
